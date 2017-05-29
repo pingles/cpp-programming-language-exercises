f48ba0ceadf5e22f5b7ce4d3a165ff792fef3e48
Paul Ingles <paul@oobaloo.co.uk>
2017-05-29 23:15:48 +0100

    first example added

---
+++ README.md
@@ -0,0 +1,6 @@
+# The C++ Programming Language
+This is the repository for my code while working through The C++ Programming Language, 4th Edition.
+
+Exercises are available here:
+* [http://www.stroustrup.com/4thExercises.pdf](http://www.stroustrup.com/4thExercises.pdf)
+
---
+++ x5/5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your name: ";
+    string name = "";
+    cin >> name;
+
+    cout << "Please enter your age: ";
+    int age = 0;
+    cin >> age;
+
+    cout << "Hello, " << name << ", you're " << age << " years old" << endl;
+
+    return 0;
+}
