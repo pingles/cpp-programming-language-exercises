99342369ab80b407d57c2ce306ffadcc1309d335
Paul Ingles <paul@oobaloo.co.uk>
2017-06-02 18:47:10 +0100

    9: open a file and write some integers

---
+++ x5/9.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+  std::ofstream out{argv[1]};
+  for (int i = 0; i < 10000; i++) {
+    out << i << std::endl;
+  }
+  return 0;
+}
